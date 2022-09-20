f30e8b0c714f7fc2a733a4c40bb54bbaf41c0f5e
leviathan4 <113945295+leviathan4@users.noreply.github.com>
2022-09-19 20:42:31 -0400

    Add files via upload

---
+++ javascript/apps.js
@@ -0,0 +1,198 @@
+document.getElementById("kalance1").onclick = function() {myFunction1()};
+function myFunction1() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/1.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance2").onclick = function() {myFunction2()};
+function myFunction2() {
+  document.getElementById("fullImg").src = "../_images/map/2.svg";
+  document.getElementById("placeName").textContent = 'Bellymena';
+  document.getElementById("fullImgBox").classList.add("hidden");
+}
+document.getElementById("kalance3").onclick = function() {myFunction3()};
+function myFunction3() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/3.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance4").onclick = function() {myFunction4()};
+function myFunction4() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/4.svg";
+  document.getElementById("placeName").textContent = 'Acrine';
+}
+document.getElementById("kalance5").onclick = function() {myFunction5()};
+function myFunction5() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/5.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance6").onclick = function() {myFunction6()};
+function myFunction6() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/6.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance7").onclick = function() {myFunction7()};
+function myFunction7() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/7.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance8").onclick = function() {myFunction8()};
+function myFunction8() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/8.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance9").onclick = function() {myFunction9()};
+function myFunction9() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/9.svg";
+  document.getElementById("placeName").textContent = 'Halivaara';
+}
+document.getElementById("kalance10").onclick = function() {myFunction10()};
+function myFunction10() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/10.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance11").onclick = function() {myFunction11()};
+function myFunction11() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/11.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance12").onclick = function() {myFunction12()};
+function myFunction12() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/12.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance13").onclick = function() {myFunction13()};
+function myFunction13() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/13.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance14").onclick = function() {myFunction14()};
+function myFunction14() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/14.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance15").onclick = function() {myFunction15()};
+function myFunction15() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/15.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance16").onclick = function() {myFunction16()};
+function myFunction16() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/16.svg";
+  document.getElementById("placeName").textContent = 'Rivershear';
+}
+document.getElementById("kalance17").onclick = function() {myFunction17()};
+function myFunction17() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/17.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance18").onclick = function() {myFunction18()};
+function myFunction18() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/18.svg";
+  document.getElementById("placeName").textContent = 'Plainsfield';
+}
+document.getElementById("kalance19").onclick = function() {myFunction19()};
+function myFunction19() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/19.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance20").onclick = function() {myFunction20()};
+function myFunction20() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/20.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance21").onclick = function() {myFunction21()};
+function myFunction21() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/21.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance22").onclick = function() {myFunction22()};
+function myFunction22() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/22.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance23").onclick = function() {myFunction23()};
+function myFunction23() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/23.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance24").onclick = function() {myFunction24()};
+function myFunction24() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/24.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance25").onclick = function() {myFunction25()};
+function myFunction25() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/25.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance26").onclick = function() {myFunction26()};
+function myFunction26() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/26.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance27").onclick = function() {myFunction27()};
+function myFunction27() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/27.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance28").onclick = function() {myFunction28()};
+function myFunction28() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/28.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance29").onclick = function() {myFunction29()};
+function myFunction29() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/29.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance30").onclick = function() {myFunction30()};
+function myFunction30() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/30.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance31").onclick = function() {myFunction31()};
+function myFunction31() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/31.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance32").onclick = function() {myFunction32()};
+function myFunction32() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/32.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
+document.getElementById("kalance33").onclick = function() {myFunction33()};
+function myFunction33() {
+   document.getElementById("fullImgBox").classList.add("hidden");
+  document.getElementById("fullImg").src = "../_images/map/33.svg";
+  document.getElementById("placeName").textContent = 'Name of Place';
+}
